d374d3a2e66d36879c47449bfa9d82c8e5364217
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2023-03-31 10:36:10 -0300

    Adicionando outra forma de resolver o problema

--- exerciciosPython/exercicioif4.py
+++ exerciciosPython/exercicioif4.py
@@ -2,9 +2,17 @@
 #exiba  uma mensagem  informando  se  é  uma vogal ou uma consoante.
 letra = input("Diite uma letra: ")
 
-letra = letra.lower()
+letra = letra.lower() #Transformando as letras em minusculas
 
 if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
     print(f"A letra {letra} é uma vogal")
 else:
     print(f"A letra {letra} é uma consoante")
+#-----------------------------------------------------------------------------------
+#outra forma de resolver o problema
+vogais = "aeiouAIUEO"
+
+if letra in vogais:
+    print('Vogal')
+else:
+    print('Consoante')
