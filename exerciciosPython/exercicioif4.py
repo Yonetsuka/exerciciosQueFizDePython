72ad49adb50186144b4b9f7c982ddca908a56804
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2023-03-31 10:31:23 -0300

    Update exercicioif4.py

--- exerciciosPython/exercicioif4.py
+++ exerciciosPython/exercicioif4.py
@@ -1,6 +1,9 @@
 #Faça  um  algoritmo  que  solicita  ao  usuário  uma  letra e  
 #exiba  uma mensagem  informando  se  é  uma vogal ou uma consoante.
 letra = input("Diite uma letra: ")
+
+letra = letra.lower()
+
 if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
     print(f"A letra {letra} é uma vogal")
 else:
