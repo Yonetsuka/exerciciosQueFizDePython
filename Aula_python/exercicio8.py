b4e3a3bcf26b7ea27f65d739ec40b58d2b14c42f
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2023-04-18 08:18:21 -0300

    Mudando exercicio 8

--- Aula_python/exercicio8.py
+++ Aula_python/exercicio8.py
@@ -1,8 +1,8 @@
 #8.Escreva  um  algoritmo  que  solicite  10  números  e  informe  qual  foi  o  
 #menor  número digitado.
 cont = 0
-menor = 99999999999999999999999999999999999999999
-while cont != 10:
+menor = int(input('Digite um numero'))
+while cont != 9:
     num = int(input('Digite um numero: '))
     if num < menor:
         menor = num
