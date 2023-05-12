5e611e77ee47c0351e8360008a3705d6752bc639
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2023-05-12 11:32:05 -0300

    atualizando exercicio 2

--- Python_variaveis/exercicio2.py
+++ Python_variaveis/exercicio2.py
@@ -2,17 +2,19 @@
 # Por exemplo: 120 é triangular, pois 4 * 5 * 6 = 120. 2730 é triangular, 
 #pois 13 * 14 * 15 = 2730. Faça uma função que receba um número inteiro e retorne True
 #se for um número triangular e False, caso contrário.
+def triangular(numero):
+    if type(numero) != int or numero < 0:
+        raise TypeError
+    
+    for a in range(numero):
+        if a * (a + 1) * (a + 2) == numero:
+            return True
+     return False
 
-
-def ver(num):
-    number1 = 0
-    number2 = 1
-    number3 = 2
-    while number3 != num:
-        if number1 + number2 + number3 == num:
-            print('O numero é triangular')
-        else:
-            print('O numero não é triangular')
-
-triangular = int(input('Digite um numero para verificar se é triangular'))
-ver(triangular)
+try:
+    numero = int(input('Informe um numero inteiro: '))
+    print(triagular(numero))
+except ValueError:
+    print('ERRO: o valor informado não é inteiro')
+except TypeError as erro:
+    print(f'ERRO: {erro}')
